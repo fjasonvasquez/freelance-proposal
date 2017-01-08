547628a18da48c323f70b0aa22c6c0a2e03de37a
fjasonvasquez <fjvasquez@aol.com>
2017-01-08 00:56:20 -0800

    Initial commit for RoR5 Microservice API Application

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+10.times do |proposal|
+	Proposal.create!(
+		customer: "Customer #{proposal}",
+		portfolio_url: 'http://portfolio.jasonvasquez.com',
+		tools: 'Ruby on Rails, Angular 2, and Postgres',
+		estimated_hours: (80 + proposal),
+		hourly_rate: 120,
+		weeks_to_complete: 12,
+		client_email: 'fjvasquez78@gmail.com',
+	)
+end
+
