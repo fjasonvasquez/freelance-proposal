6591bf4ad4cfb39ff9c69dc2ad02d10a8669ad6f
fjasonvasquez <fjvasquez@aol.com>
2017-01-15 23:40:50 -0800

    Sending emails from the Rails Microservice

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,5 @@
 	)
 end
 
+Proposal.create!(customer: "Google", portfolio_url: 'http://portfolio.jasonvasquez.com', tools: 'Ruby on Rails, Angular 2, and Postgres', estimated_hours: 120, hourly_rate: 120, weeks_to_complete: 12, client_email: 'fjvasquez78@gmail.com')
+
